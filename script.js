6022a301d4c4800829cc00a466c5e2828c679216
kuratadeesu <96030065+kuratadeesu@users.noreply.github.com>
2025-11-14 23:22:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let score = 0;
+let gameInterval;
+let time = 10;
+let timeInterval;
+let moleTimer;
+
+$(function () {
+
+    $("#startBtn").on("click", function () {
+        startGame();
+    });
+
+    $(".mole").on("click touchstart", function () {
+        if ($(this).parent().hasClass("active")) {
+            score++;
+            $("#score").text("Score: " + score);
+            $(this).parent().removeClass("active");
+        }
+    });
+
+});
+
+function startGame() {
+    // スコア＆時間初期化
+    score = 0;
+    time = 10;
+
+    $("#score").text("Score: " + score);
+    $("#time").text("Time: " + time);
+
+    // いったん全て停止
+    clearInterval(gameInterval);
+    clearInterval(timeInterval);
+    clearTimeout(moleTimer);
+    $(".hole").removeClass("active");
+
+    // モグラ出現開始
+    gameInterval = setInterval(showRandomMole, 900);
+
+    // 10 秒カウント開始
+    timeInterval = setInterval(() => {
+        time--;
+        $("#time").text("Time: " + time);
+
+        if (time <= 0) {
+            endGame();
+        }
+    }, 1000);
+}
+
+function showRandomMole() {
+    $(".hole").removeClass("active");
+
+    const index = Math.floor(Math.random() * 9);
+    const hole = $(".hole").eq(index);
+    hole.addClass("active");
+
+    // モグラが出てる時間を制御
+    clearTimeout(moleTimer);
+    moleTimer = setTimeout(() => {
+        hole.removeClass("active");
+    }, 300);
+}
+
+function endGame() {
+    clearInterval(gameInterval);
+    clearInterval(timeInterval);
+    clearTimeout(moleTimer);
+    $(".hole").removeClass("active");
+
+    alert("終了！あなたのスコアは " + score + " 点です！");
+}
